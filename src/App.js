099767acaf91f41a55dcba923f5b1c2a75148673
dolong0702 <longngocjddoox@gmail.com>
2023-01-27 00:50:24 +0700

    Bài 59: cài đặt babel-plugin-module-resolver

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
+import Buttom from '~/components/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+    <Buttom />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/Button/index.js
@@ -0,0 +1,7 @@
+import myAlert from "~/myAlert"
+
+function Buttom() {
+    return <button onClick={myAlert}>Click me!</button>
+}
+
+export default Buttom
