23eb7bcf4612d55ed66341462327c058d746ff79
dolong0702 <longngocjddoox@gmail.com>
2023-01-27 22:05:46 +0700

    Cài đặt sass

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
-import logo from './logo.svg';
-import Buttom from '~/components/Button';
-import './App.css';
-console.log('hello các bạn')
 function App() {
-  return (
-    <div className="App">
-    <Buttom />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hello anh em</h1>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Button/index.js
+++
@@ -1,7 +0,0 @@
-import myAlert from "~/myAlert"
-
-function Buttom() {
-    return <button onClick={myAlert}>Click me!</button>
-}
-
-export default Buttom
